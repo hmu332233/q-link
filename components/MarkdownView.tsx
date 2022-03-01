477bb1604c812ef7b1cbad8d2a381eb9264f55c4
minung.han <hmu332233@gmail.com>
2022-03-02 00:05:52 +0900

    inline code에도 하이라이트 적용

--- components/MarkdownView.tsx
+++ components/MarkdownView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 type Props = {
   contents: string;
 };
@@ -8,7 +9,32 @@ type Props = {
 function MarkdownView({ contents }: Props) {
   return (
     <article className="prose w-full max-w-lg">
-      <ReactMarkdown>{contents}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            if (inline) {
+              return (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            }
+
+            const [, language] = /language-(\w+)/.exec(className || '') || [];
+            return (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={atomDark}
+                language={language || 'text'}
+                PreTag="div"
+                {...props}
+              />
+            );
+          },
+        }}
+      >
+        {contents}
+      </ReactMarkdown>
     </article>
   );
 }
