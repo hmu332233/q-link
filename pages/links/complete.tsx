7d3785fbbf87cdd7fd8723baa15572e325cdd838
minung.han <hmu332233@gmail.com>
2022-03-03 02:07:03 +0900

    복사하기 기능 추가

--- pages/links/complete.tsx
+++ pages/links/complete.tsx
@@ -1,20 +1,35 @@
-import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import Steps from 'components/Steps';
-
-type Props = {};
+import { useState } from 'react';
 
 function LinkComplete() {
+  const [copied, setCopied] = useState(false);
   const router = useRouter();
   const {
-    query: { link },
+    query: { link = '' },
   } = router;
+
+  const handleCopyClick = async () => {
+    await navigator.clipboard.writeText(link as string);
+    setCopied(true);
+  };
+
   return (
     <div className="flex flex-col items-center gap-y-4">
       <Steps step={3} />
-      <a className="link link-accent">{link}</a>
-      <button className="btn btn-wide">복사하기</button>
+      <Link href={link as string} passHref>
+        <a className="link link-accent">{link}</a>
+      </Link>
+      <div className="indicator">
+        {copied && (
+          <span className="indicator-item badge badge-secondary">복사됨</span>
+        )}
+        <button className="btn btn-wide" onClick={handleCopyClick}>
+          복사하기
+        </button>
+      </div>
     </div>
   );
 }
