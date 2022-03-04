acfae80fddf5f03c017d47e67086dce187b16974
minung.han <hmu332233@gmail.com>
2022-03-05 02:53:11 +0900

    Q.Link 설명 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,17 @@ function Index() {
       </div>
 
       <article className="prose">
-        <h3>Quiz Link는 무엇인가요?</h3>
+        <h3>
+          <span className="text-primary">Q.</span>Link는 무엇인가요?
+        </h3>
         <p>
-          Quiz Link란 퀴즈의 정답을 맞췄을 때만 링크에 접속 할 수 있도록 해주는
-          간단한 서비스입니다.
-          <br />
-          이벤트, 구인 페이지, 모바일 청접장등 여러 상황에서 사용해보세요!
+          <span className="font-bold">
+            <span className="text-primary">Q.</span>Link
+          </span>
+          란 <span className="underline">퀴즈로 접근하는 링크</span>라는 의미로
+          퀴즈의 정답을 맞췄을 때만 링크에 접속 할 수 있도록 해주는 간단한
+          서비스입니다. 이벤트, 구인 페이지, 모바일 청접장등 여러 상황에서
+          사용해보세요!
           <br />이 프로젝트는{' '}
           <a
             className="link"
@@ -38,11 +43,11 @@ function Index() {
         </p>
         <h3>어떤 상황에서 사용하나요?</h3>
         <p>
-          간단한 프로그래밍 문제를 푼 사람만 지원을 할 수 있도록 하기
+          간단한 문제를 푼 사람만 지원을 할 수 있도록 하기
           <br />
           생일을 알고 있는 사람만 모바일 초대장을 볼 수 있도록 하기
           <br />
-          어뷰징성 접근을 막기
+          비밀번호를 입력한 사람만 특정 API를 호출되도록 하기
           <br />
           등등 간단하지만 다양한 상황에서 활용해볼 수 있습니다.
         </p>
