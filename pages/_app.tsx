612cde09a02b3060f91f208cd1049b4b8645b7d4
minung.han <hmu332233@gmail.com>
2022-03-05 02:27:56 +0900

    gtag를 이용한 ga 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import Layout from 'components/Layout';
+import * as gtag from 'libs/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 function Document() {
   return (
     <Html data-theme="bumblebee">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GA_ID}');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-};
+  );
+}
 
 export default Document;
