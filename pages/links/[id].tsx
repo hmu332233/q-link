7b99c1a80e5dd797488c346cbba9b9e2b6a073ed
minung.han <hmu332233@gmail.com>
2022-03-07 02:04:40 +0900

    퀴즈 페이지 title에 link id가 포함되도록 변경

--- pages/links/[id].tsx
+++ pages/links/[id].tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps } from 'next';
 import Link from 'next/link';
+import Head from 'next/head';
 
 import axios from 'axios';
 
@@ -8,12 +9,13 @@ import DefaultInput from 'components/DefaultInput';
 import { useEffect, useState } from 'react';
 
 type Props = {
+  id: string;
   url: string;
   contents: string;
   correct: string;
 };
 
-function LinkId({ url, contents, correct }: Props) {
+function LinkId({ id, url, contents, correct }: Props) {
   const [isCorrect, setIsCorrect] = useState(false);
   const [showFeedback, setShowFeedback] = useState(false);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -38,22 +40,27 @@ function LinkId({ url, contents, correct }: Props) {
     : '오답입니다.';
 
   return (
-    <div className="flex flex-col items-center gap-y-4">
-      <MarkdownView contents={contents} />
-      <form className="w-full max-w-lg" id="answer" onSubmit={handleSubmit}>
-        <DefaultInput
-          name="answer"
-          placeholder="정답을 입력해주세요."
-          feedback={showFeedback ? feedbackMessage : undefined}
-        />
-      </form>
-      <button className="btn btn-wide" type="submit" form="answer">
-        확인
-      </button>
-      <Link href="/links/new">
-        <a className="link">링크 직접 만들어보기</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Q.Link - {id}</title>
+      </Head>
+      <div className="flex flex-col items-center gap-y-4">
+        <MarkdownView contents={contents} />
+        <form className="w-full max-w-lg" id="answer" onSubmit={handleSubmit}>
+          <DefaultInput
+            name="answer"
+            placeholder="정답을 입력해주세요."
+            feedback={showFeedback ? feedbackMessage : undefined}
+          />
+        </form>
+        <button className="btn btn-wide" type="submit" form="answer">
+          확인
+        </button>
+        <Link href="/links/new">
+          <a className="link">링크 직접 만들어보기</a>
+        </Link>
+      </div>
+    </>
   );
 }
 
@@ -67,6 +74,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const { url, contents, correct } = data;
   return {
     props: {
+      id,
       url,
       contents,
       correct,
