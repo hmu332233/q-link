78f7d71af55cf9488f86fa325d1a29cd9538670f
Minung Han <hmu332233@gmail.com>
2022-05-06 19:42:26 +0900

    @import -> link 방식으로 웹폰트 로딩 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,9 +19,15 @@ function Document() {
           }}
         />
         <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin=""
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+        {/* <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </Head>
       <body>
         <Main />
