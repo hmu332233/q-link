0aae84535bf1e9b1fa1f9a2bb4c6af1ad27cd52e
Minung Han <hmu332233@gmail.com>
2022-05-16 00:33:37 +0900

    seo locale 처리

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,23 +1,18 @@
 import Head from 'next/head';
-import React from 'react';
+import { useTranslation } from 'next-i18next';
 
 type Props = {};
 
 function Seo({}: Props) {
+  const { t } = useTranslation();
   return (
     <Head>
       <title>Q.Link</title>
-      <meta name="description" content="퀴즈로 접근하는 링크, Q.Link입니다!" />
-      <meta
-        name="keywords"
-        content="Q.Link,퀴즈 링크,링크,문제,Quiz Link,Link,Quiz"
-      />
+      <meta name="description" content={t('common:seo.description')} />
+      <meta name="keywords" content={t('common:seo.keywords')} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Q.Link" />
-      <meta
-        property="og:description"
-        content="퀴즈로 접근하는 링크, Q.Link입니다!"
-      />
+      <meta property="og:description" content={t('common:seo.description')} />
       {/* <meta property="og:image" content=""/> */}
       <meta property="og:url" content="https://q-link.minung.dev" />
     </Head>
--- public/locales/ko/common.js
+++ public/locales/ko/common.js
@@ -3,4 +3,8 @@ module.exports = {
   submit: '제출',
   share: '공유하기',
   copied: '링크가 복사되었습니다.',
+  seo: {
+    description: '퀴즈로 접근하는 링크, Q.Link입니다!',
+    keywords: 'Q.Link,퀴즈 링크,링크,문제,Quiz Link,Link,Quiz',
+  },
 };
