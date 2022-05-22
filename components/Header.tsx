26d29bfb9317b2db082b7507c1495c890d55fec3
Minung Han <hmu332233@gmail.com>
2022-05-22 22:17:27 +0900

    언어 선택 UI 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 
+import LanguageSelector from 'components/LanguageSelector';
+
 type Props = {};
 
 function Header({}: Props) {
   return (
-    <header className="container text-center py-4">
-      <h1 className="text-4xl font-bold text-base-content ">
+    <header className="p-4 flex items-center relative">
+      <h1 className="text-4xl font-bold text-center text-base-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <Link href="/">
           <a>
             <span className="text-primary">Q.</span>Link
           </a>
         </Link>
       </h1>
+      <LanguageSelector className="ml-auto" />
     </header>
   );
 }
---
+++ components/LanguageSelector.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import cn from 'classnames';
+
+const ITEMS = [
+  {
+    locale: 'en',
+    text: 'English',
+  },
+  {
+    locale: 'ko',
+    text: '한국어',
+  },
+];
+
+type Props = {
+  className?: string;
+};
+
+function LanguageSelector({ className }: Props) {
+  const router = useRouter();
+
+  return (
+    <div className={cn(className, 'dropdown dropdown-end')}>
+      <label tabIndex={0} className="btn btn-ghost m-1">
+        Language
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu bg-base-100 w-56 rounded-box shadow"
+      >
+        {ITEMS.map((item) => (
+          <li key={item.locale}>
+            <Link href={router.asPath} locale={item.locale}>
+              <a className={cn(router.locale === item.locale && 'active')}>
+                {item.text}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default LanguageSelector;
