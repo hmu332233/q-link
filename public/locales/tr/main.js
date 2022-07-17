d4fa12c6b30b486de0be6525dc05e9d144ca5269
Erol Bicer <33051763+nucerl@users.noreply.github.com>
2022-07-18 08:54:20 +0900

    copied main.js to tr locale

---
+++ public/locales/tr/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  description: 'Hide your link behind a simple quiz.',
+  button: {
+    create: 'Create Q.Link',
+    what: `What's Q.Link?`,
+    demo: 'Try Demo',
+  },
+  demoUrl: '/links/62825d42dad9a9d8d439aa54',
+};
