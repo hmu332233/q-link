986fd18cf5579e0f0168db975170ef1e69706fc0
Erol Bicer <33051763+nucerl@users.noreply.github.com>
2022-07-18 08:53:13 +0900

    copied link.js to tr locale

---
+++ public/locales/tr/link.js
@@ -0,0 +1,21 @@
+module.exports = {
+  feedback: {
+    correct: 'Correct! Go to the link after 5 seconds.',
+    fail: 'Incorrect answer.',
+  },
+  steps: {
+    one: 'Enter Link',
+    two: 'Create Quiz',
+    three: 'Complete',
+  },
+  stepBody: {
+    step1: {
+      feedback1: `Enter a link to move when the answer is correct.<br />If you don't have a link to move, try using <4 className="link" href="https://celebration.minung.dev" target="_blank" rel="noreferrer">celebration.minung.dev</4>`,
+    },
+    step2: {
+      feedback1: 'Enter the correct answer.',
+      feedback2: 'Enter the quiz. (Markdown support)',
+      example: `### Quiz Example\n\nMarkdown grammar is available.\n\n\`\`\`\n1 + 2 + 3 + 4 = ?\n\`\`\``,
+    },
+  },
+};
