b65101b151d0c7ee700190f7855a02080f859b55
Erol Bicer <33051763+nucerl@users.noreply.github.com>
2022-07-18 08:46:25 +0900

    duplicated common.js

---
+++ public/locales/common.js
@@ -0,0 +1,14 @@
+module.exports = {
+  next: 'Next',
+  submit: 'Submit',
+  share: 'Share',
+  copied: 'The link has been copied!',
+  contents: {
+    answerPlaceholder: 'Enter the answer',
+    createLink: 'Create your Q.Link',
+  },
+  seo: {
+    description: 'Hide your link behind a simple quiz.',
+    keywords: 'Q.Link,퀴즈 링크,링크,문제,Quiz Link,Link,Quiz',
+  },
+};
