ee77549ff13acd518bfba1400a51c74b3f17fc32
Minung Han <hmu332233@gmail.com>
2022-09-18 01:25:47 +0900

    feat: container-fluid 옵션 추가

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import cn from 'classnames';
+
 import Seo from 'components/Seo';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -7,15 +9,18 @@ import Footer from 'components/Footer';
 type Props = {
   children: React.ReactNode;
   hideHeader?: boolean;
+  fluid?: boolean;
 };
 
-function Layout({ children, hideHeader }: Props) {
+function Layout({ children, hideHeader, fluid }: Props) {
   return (
     <>
       <Seo />
       <div className="flex flex-col min-h-screen">
         {hideHeader || <Header />}
-        <main className="container grow">{children}</main>
+        <main className={cn('grow', fluid ? 'container-fluid' : 'container')}>
+          {children}
+        </main>
         <Footer />
       </div>
     </>
